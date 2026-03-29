643c63c0528b977435629629c8f154bf87dadb74
AbdullahAhmed6859 <aa09303@st.habib.edu.pk>
2026-03-29 13:02:25 +0500

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { type Request, type Response } from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
